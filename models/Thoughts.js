e21450264f2ea413f9959a08c656c5986b951790
Ben Smerd <bsmerd@adam.com.au>
2022-10-06 22:07:27 +1030

    started thoughts model

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,4 +1,17 @@
+const { Schema, model } = require('mongoose');
 
-
+// Thoughts schema
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280,
+    }, 
+    createdAt: {
+        type: Date,
+        default: () => Date.now(),
+    }
+})
 
 /// make the model named 'Thoughts"
--- models/Users.js
+++ models/Users.js
@@ -13,18 +13,21 @@ const userSchema = new Schema({
         unique: true,
         required: true,
         trim: true,
-    }, email: {
+    }, 
+    email: {
         type: String,
         unique: true,
         required: true,
         validate: [validateEmail, 'Please fill a valid email address'],
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
-    }, thoughts: [
+    }, 
+    thoughts: [
         {
             type: Schema.Types.ObjectId,
             ref: 'Thoughts',
         },
-    ], friends: [this]
+    ], 
+    friends: [this]
     }, {
         toJSON: {
             virtuals: true,
