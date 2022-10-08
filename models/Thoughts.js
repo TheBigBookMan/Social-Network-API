cb2c5f07dbb24e7bc4229863a53e4729c8adccb6
Ben Smerd <bsmerd@adam.com.au>
2022-10-08 19:09:08 +1030

    added reaction schema to thoughts model

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -35,7 +35,23 @@ const Thoughts = model('thoughts', thoughtSchema);
 
 // Schema for reactions subdocument of Thoughts
 const reactionsSchema = new Schema({
-    
+    reactionID: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: () => Date.now(),
+    }
 })
 
 module.exports = Thoughts;
